0389aa32b6589ee4411372a28181b9406625d9b8
andres0818 <c.andres.perez.h@gmail.com>
2023-04-09 19:07:34 -0500

    funtion reload,  paused  and layout score text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,53 @@
 import React from "react";
-import { StyleSheet, View } from "react-native/types";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Colors } from "../styles/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 interface HeaderProps {
   reloadGame: () => void;
   pauseGame: () => void;
   children: JSX.Element;
-  isPause: boolean;
+  isPaused: boolean;
 }
 
 const Header = ({
   children,
   reloadGame,
   pauseGame,
-  isPause,
+  isPaused,
 }: HeaderProps): JSX.Element => {
-  return <View style={styles.container}>{children}</View>;
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={reloadGame}>
+        <Ionicons name="reload-circle" size={35} color={Colors.primary} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={pauseGame}>
+        <FontAwesome
+          name={isPaused ? "play-circle" : "pause-circle"}
+          size={35}
+          color={Colors.primary}
+        />
+      </TouchableOpacity>
+      {children}
+    </View>
+  );
 };
 
 export default Header;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 0.05,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     borderColor: Colors.primary,
     borderWidth: 12,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    borderBottomWidth: 0,
+    padding: 15,
+    backgroundColor: Colors.background,
   },
 });
